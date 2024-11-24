453195f00bc4ccb1189a9d5008e883c9d5b0907a
Putr4ari <148017593+Putr4ari@users.noreply.github.com>
2024-11-25 01:09:11 +0700

    Update LaporanDAO.java

--- DAO/LaporanDAO.java
+++ DAO/LaporanDAO.java
@@ -2,11 +2,14 @@ package dao;
 
 import model.Laporan;
 import java.util.List;
+import model.Pekerja;
 
 public interface LaporanDAO {
     void addLaporan(Laporan laporan);
     List<Laporan> getAllLaporan();
     Laporan getLaporanByID(int idLaporan);
     void updateLaporan(Laporan laporan);
-    void deleteLaporan(int idLaporan);
+    void deleteLaporan(int idLaporan);
+
+    public List<Pekerja> getAllPekerja();
 }
