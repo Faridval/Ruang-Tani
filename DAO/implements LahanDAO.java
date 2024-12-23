26dc362b83ea888915b7c8915ca2df4f42445e3f
Faridval <148364360+Faridval@users.noreply.github.com>
2024-12-23 07:13:55 +0700

    Update implements LahanDAO.java

--- DAO/implements LahanDAO.java
+++ DAO/implements LahanDAO.java
@@ -5,6 +5,7 @@ import model.Lahan;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.image.Image;
 
 public class LahanDAOImpl implements LahanDAO {
     private Connection connection;
@@ -15,12 +16,15 @@ public class LahanDAOImpl implements LahanDAO {
 
     @Override
     public void addLahan(Lahan lahan) {
-        String sql = "INSERT INTO Lahan (Lokasi, Luas, Jenis_lahan, ID_Pemilik) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO Lahan (Lokasi, Luas, Jenis_lahan, jenis_bibit, ID_Pemilik, Status_lahan, image) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, lahan.getLokasi());
             statement.setDouble(2, lahan.getLuas());
             statement.setString(3, lahan.getJenisLahan());
-            statement.setInt(4, lahan.getIdPemilik());
+            statement.setString(4, lahan.getJenisBibit());
+            statement.setInt(5, lahan.getIdPemilik());
+            statement.setString(6, lahan.getStatusLahan());  // Tambahkan statusLahan
+            statement.setString(7, (String) lahan.getImage());
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -36,10 +40,11 @@ public class LahanDAOImpl implements LahanDAO {
             while (resultSet.next()) {
                 Lahan lahan = new Lahan();
                 lahan.setIdLahan(resultSet.getInt("ID_Lahan"));
-                lahan.setStatusLahan(resultSet.getString("Status_lahan"));
+                lahan.setStatusLahan(resultSet.getString("Status_lahan"));  // Ambil statusLahan
                 lahan.setLokasi(resultSet.getString("Lokasi"));
                 lahan.setLuas(resultSet.getDouble("Luas"));
                 lahan.setJenisLahan(resultSet.getString("Jenis_lahan"));
+                lahan.setImage(resultSet.getString("image"));
                 lahan.setIdPemilik(resultSet.getInt("ID_Pemilik"));
                 lahanList.add(lahan);
             }
@@ -59,11 +64,12 @@ public class LahanDAOImpl implements LahanDAO {
                 if (resultSet.next()) {
                     lahan = new Lahan();
                     lahan.setIdLahan(resultSet.getInt("ID_Lahan"));
-                    lahan.setStatusLahan(resultSet.getString("Status_lahan"));
+                    lahan.setStatusLahan(resultSet.getString("Status_lahan"));  // Ambil statusLahan
                     lahan.setLokasi(resultSet.getString("Lokasi"));
                     lahan.setLuas(resultSet.getDouble("Luas"));
                     lahan.setJenisLahan(resultSet.getString("Jenis_lahan"));
                     lahan.setIdPemilik(resultSet.getInt("ID_Pemilik"));
+                    lahan.setImage(resultSet.getString("image"));
                 }
             }
         } catch (SQLException e) {
@@ -71,23 +77,25 @@ public class LahanDAOImpl implements LahanDAO {
         }
         return lahan;
     }
-
-    @Override
-    public void updateLahan(Lahan lahan) {
-        String sql = "UPDATE Lahan SET Status_lahan = ?, Lokasi = ?, Luas = ?, Jenis_lahan = ?, ID_Pemilik = ? WHERE ID_Lahan = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, lahan.getStatusLahan());
-            statement.setString(2, lahan.getLokasi());
-            statement.setDouble(3, lahan.getLuas());
-            statement.setString(4, lahan.getJenisLahan());
-            statement.setInt(5, lahan.getIdPemilik());
-            statement.setInt(6, lahan.getIdLahan());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+@Override
+public void updateLahan(Lahan lahan) {
+    String sql = "UPDATE Lahan SET Lokasi = ?, Luas = ?, Jenis_lahan = ?, Status_lahan = ?, Jenis_Bibit = ?, image = ?, ID_Pemilik = ? WHERE ID_Lahan = ?";
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+        statement.setString(1, lahan.getLokasi());
+        statement.setDouble(2, lahan.getLuas());
+        statement.setString(3, lahan.getJenisLahan());
+        statement.setString(4, lahan.getStatusLahan());
+        statement.setString(5, lahan.getJenisBibit());
+        statement.setString(6, lahan.getImage()); // Pastikan tipe datanya sesuai
+        statement.setInt(7, lahan.getIdPemilik());
+        statement.setInt(8, lahan.getIdLahan());
+        statement.executeUpdate();
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
+}
 
+    
     @Override
     public List<Lahan> getAllLahanByUsrID(int usrID) {
         List<Lahan> lahanList = new ArrayList<>();
@@ -98,11 +106,12 @@ public class LahanDAOImpl implements LahanDAO {
                 while (resultSet.next()) {
                     Lahan lahan = new Lahan();
                     lahan.setIdLahan(resultSet.getInt("ID_Lahan"));
-                    lahan.setStatusLahan(resultSet.getString("Status_lahan"));
+                    lahan.setStatusLahan(resultSet.getString("Status_lahan"));  // Ambil statusLahan
                     lahan.setLokasi(resultSet.getString("Lokasi"));
                     lahan.setLuas(resultSet.getDouble("Luas"));
                     lahan.setJenisLahan(resultSet.getString("Jenis_lahan"));
                     lahan.setIdPemilik(resultSet.getInt("ID_Pemilik"));
+                    lahan.setImage(resultSet.getString("image"));
                     lahanList.add(lahan);
                 }
             }
@@ -111,7 +120,7 @@ public class LahanDAOImpl implements LahanDAO {
         }
         return lahanList;
     }
-    
+
     @Override
     public String deleteLahan(int idLahan, int idPemilik) {
         String sql = "DELETE FROM Lahan WHERE ID_Lahan = ? AND ID_Pemilik = ?";
@@ -123,11 +132,12 @@ public class LahanDAOImpl implements LahanDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        if (status == 1){
+        if (status == 1) {
             return "success";
-        }
-        else {
+        } else {
             return "failed";
         }
     }
 }
+
+
