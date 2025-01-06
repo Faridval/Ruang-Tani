4dedba0707c23777fec7fb0fc39a70d214719d33
Putr4ari <148017593+Putr4ari@users.noreply.github.com>
2025-01-06 23:18:44 +0700

    Update Lahan.java

--- Model/Lahan.java
+++ Model/Lahan.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.List;
+
 public class Lahan {
     private int idLahan;
     private String statusLahan;
@@ -8,25 +10,38 @@ public class Lahan {
     private String jenisLahan;
     private String jenisBibit;
     private int idPemilik;
+    private int idPekerja;
     private String image;
-    
-    public Lahan(){
+    private Pekerja pekerja;
+    private Pemilik pemilik;
+    private List<Laporan> laporanList;
+
+    public Lahan() {
     }
 
-    // Konstruktor untuk menginisialisasi semua atribut
-    public Lahan(int idLahan, String statusLahan, String lokasi, double luas, String jenisLahan, String jenisBibit, int idPemilik, String image) {
+    public Lahan(int idLahan, String statusLahan, String lokasi, double luas, 
+                 String jenisLahan, String jenisBibit, String image, Pemilik pemilik, Pekerja pekerja) {
         this.idLahan = idLahan;
         this.statusLahan = statusLahan;
         this.lokasi = lokasi;
         this.luas = luas;
         this.jenisLahan = jenisLahan;
         this.jenisBibit = jenisBibit;
-        this.idPemilik = idPemilik;
         this.image = image;
-        
+        this.pemilik = pemilik;
+        this.pekerja = pekerja;
     }
 
-    // Getter dan Setter (Opsional, jika diperlukan)
+    public Lahan(Pemilik pemilik, Pekerja pekerja, String lokasi) {
+        this.pemilik = pemilik;
+        this.pekerja = pekerja;
+        this.lokasi = lokasi;
+    }
+
+    public Lahan(int aInt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
     public int getIdLahan() {
         return idLahan;
     }
@@ -83,11 +98,47 @@ public class Lahan {
         this.idPemilik = idPemilik;
     }
 
+    public int getIdPekerja() {
+        return idPekerja;
+    }
+
+    public void setIdPekerja(int idPekerja) {
+        this.idPekerja = idPekerja;
+    }
+
     public String getImage() {
         return image;
     }
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public Pekerja getPekerja() {
+        return pekerja;
+    }
+
+    public void setPekerja(Pekerja pekerja) {
+        this.pekerja = pekerja;
+    }
+
+    public Pemilik getPemilik() {
+        return pemilik;
+    }
+
+    public void setPemilik(Pemilik pemilik) {
+        this.pemilik = pemilik;
+    }
+
+    public List<Laporan> getLaporanList() {
+        return laporanList;
+    }
+
+    public void setLaporanList(List<Laporan> laporanList) {
+        this.laporanList = laporanList;
+    }
+
+    public int getId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
