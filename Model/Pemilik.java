fea3e727216e1617e967bfdcb5dc3c9d54ce92a5
Putr4ari <148017593+Putr4ari@users.noreply.github.com>
2025-01-06 23:26:31 +0700

    Update Pemilik.java

--- Model/Pemilik.java
+++ Model/Pemilik.java
@@ -1,29 +1,74 @@
 package model;
 
 public class Pemilik extends User {
-    private int id;
-    
-    public Pemilik(String username, String password, String nama, String alamat, String pemilik) {
-        super(username, password, nama, alamat, "Pemilik"); // Set role sebagai "Pemilik"
+
+    private String nomorKtp;
+    private String nomorHp;
+    private Integer idPemilik;
+
+    public Pemilik(int aInt, String username, String password, String nama, String alamat, String role) {
+        super(username, password, nama, alamat, role);
+        this.nomorKtp = "";
+        this.nomorHp = "";
     }
 
-    public Pemilik(String username, String password, String nama, String alamat, int idPemilik) {
+    public Pemilik(String username, String password, String nama, String alamat, Integer idPemilik, String nomorKtp, String nomorHp) {
+        super(username, password, nama, alamat, "Pemilik");
+        this.idPemilik = idPemilik;
+        this.nomorKtp = nomorKtp;
+        this.nomorHp = nomorHp;
+    }
+        public Pemilik(String username, String password, String nama, String alamat, int idPemilik) {
         super(username, password, nama, alamat, idPemilik);
         setRole("Pemilik"); // Set role sebagai "Pemilik"
     }
-    public Pemilik(int id, String username, String password, String nama, String alamat, String role) {
-        super(username, password, nama, alamat, role); // Panggil konstruktor superclass
-        this.id = id; // Tetapkan ID pemilik
+
+    public Pemilik(int aInt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    // Getter dan Setter untuk ID
-    @Override
-    public int getId() {
-        return id;
+    public Integer getIdPemilik() {
+        return idPemilik;
+    }
+
+    public void setIdPemilik(Integer idPemilik) {
+        this.idPemilik = idPemilik;
+    }
+
+    public String getNomorKtp() {
+        return nomorKtp;
+    }
+
+    public void setNomorKtp(String nomorKtp) {
+        this.nomorKtp = nomorKtp;
+    }
+
+    public String getNomorHp() {
+        return nomorHp;
+    }
+
+    public void setNomorHp(String nomorHp) {
+        this.nomorHp = nomorHp;
+    }
+
+    public String getNamaPemilik() {
+        return getNama();
     }
 
     @Override
-    public void setId(int id) {
-        this.id = id;
+    public String toString() {
+        return "Pemilik{" +
+                "idPemilik=" + idPemilik +
+                ", nomorKtp='" + nomorKtp + '\'' +
+                ", nomorHp='" + nomorHp + '\'' +
+                ", nama='" + getNama() + '\'' +
+                ", alamat='" + getAlamat() + '\'' +
+                '}';
     }
+    
+    public String getNamaPemilikLahan() {
+        return this.getNamaPemilik();
+    }
+}
+
 }
